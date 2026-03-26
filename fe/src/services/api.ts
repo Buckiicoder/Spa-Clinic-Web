7f469b87cdc509aec21f0efa0e59657dd6e1df26
bucky <phamntn12@gmail.com>
2026-03-26 16:08:23 +0700

    Change Api URL from local to render p2

--- fe/src/services/api.ts
+++ fe/src/services/api.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
+  // local url
+  // baseURL: 'http://localhost:5000/api',
+  
   baseURL: 'https://spa-clinic-web.onrender.com',
   withCredentials: true
 })
